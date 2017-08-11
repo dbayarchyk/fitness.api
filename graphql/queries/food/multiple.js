272dda806f43960b501f0de49ccf02a8bcb10fb8
Dmitry Boyarchik <dima97boyarchik@gmail.com>
2017-08-11 15:18:30 +0300

    Added query params to foods query

--- graphql/queries/food/multiple.js
+++ graphql/queries/food/multiple.js
@@ -1,18 +1,36 @@
 import {
   GraphQLList,
-  GraphQLID
+  GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLString
 } from 'graphql';
 import { foodType } from '../../types/food';
 import FoodModel from '../../../models/food';
 
 export default {
   type: new GraphQLList(foodType),
+  args: {
+    query: {
+      name: 'query',
+      type: new GraphQLInputObjectType({
+        name: 'FoodQueryParams',
+        description: 'Food query params',
+        fields: () => ({
+          name:      { type: GraphQLString, defaultValue: '' },
+          category:  { type: GraphQLString, defaultValue: '' }
+        })
+      })
+    }
+  },
   resolve(root, params, context) {
     if (!context.user) {
       throw new Error('You have not access');
     }
 
-    const users = FoodModel.find().exec();
+    const foods = FoodModel.find({
+      name: { '$regex': params.query.name, '$options': 'i' } ,
+      category: { '$regex': params.query.category, '$options': 'i' }
+    }).exec();
 
     if (!foods) {
       throw new Error('Error while fetching foods...');
